1ce7ce89fded8b1167a30e10ac2d42ba3fd909f3
amin havasi <pau.ahq@gmail.com>
2020-04-28 13:08:51 +0430

    main route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
 import Login from './components/login';
+import Home from './components/homePage';
+import NotFound from './components/notFound';
 const App = () => {
     return (
         <Switch>
-            <Route exact path="/" component={null} />
+            <Route exact path="/" component={Home} />
             <Route path="/login" component={Login} />
-            <Redirect to="/" />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect to="/not-found" />
         </Switch>
     );
 };
