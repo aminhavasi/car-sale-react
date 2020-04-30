d747ce5241ea9d63eeb1a4f02a4f712b1ca639cf
amin havasi <pau.ahq@gmail.com>
2020-04-30 10:04:08 +0430

    some update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
 import apolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import Login from './components/login';
 import Home from './components/homePage';
 import NotFound from './components/notFound';
 
 const client = new apolloClient({
-    uri: 'http://localhost:8000/graphQl',
+    uri: 'http://localhost:8000/graphQl ',
 });
 const App = () => {
     return (
-        <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
-            <Route path="/not-found" component={NotFound} />
-            <Redirect to="/not-found" />
-        </Switch>
+        <ApolloProvider client={client}>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/login" component={Login} />
+                <Route path="/not-found" component={NotFound} />
+                <Redirect to="/not-found" />
+            </Switch>
+        </ApolloProvider>
     );
 };
 
--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
 
-const Home = () => {
-    return <div>Home Page</div>;
+const getName = gql`
+    {
+        car {
+            name
+            id
+        }
+    }
+`;
+
+const Home = (props) => {
+    const data = props.data;
+    if (data.loading) {
+        return <div>Loading ...</div>;
+    }
+    return (
+        <div>
+            {data.car.map((c) => (
+                <p key={c.id}>{c.name}</p>
+            ))}
+        </div>
+    );
 };
 
-export default Home;
+export default graphql(getName)(Home);
