94d55fd4feb32d6ee18c0f0441d31cb1d5eab6fd
amin havasi <pau.ahq@gmail.com>
2020-05-16 14:10:09 +0430

    text

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -4,9 +4,20 @@ import './../css/homePage.css';
 const Home = () => {
     return (
         <React.Fragment>
-            <div className="jumbotron bg">
-                <h1>Bootstrap Tutorial</h1>
-                <p>Bootstrap is the most popular HTML, CSS...</p>
+            <div className="jumbotron bg jumbotron-fluid text-white d-flex justify-content-center aligin-items-center ">
+                <div className="container text-center">
+                    <h1 className="text-primary display-1 text-uppercase">
+                        amin khodro
+                    </h1>
+                    <p
+                        className="lead text-white  text-shadow"
+                        style={{
+                            backgroundColor: ' rgba(5, 0, 0, 0.2) ',
+                        }}
+                    >
+                        شرکت خرید و فروش خودرو های ایرانی سهامی عام
+                    </p>
+                </div>
             </div>
         </React.Fragment>
     );
