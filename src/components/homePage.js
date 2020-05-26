e348b3558b54d5166601224f545f8997564cfe93
amin havasi <pau.ahq@gmail.com>
2020-05-26 20:40:26 +0430

    dfdf

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -40,7 +40,7 @@ const Home = () => {
             </div>
             <div className="d-flex ">
                 <div className="container inline cars">
-                    <div className="lam text-white mr-auto ">asss</div>
+                    <div className="lam text-white mr-auto د ">asss</div>
                     <div className="lam text-white mx-auto">slaam</div>
                     <div className="lam text-white ml-auto ">aیmin</div>
                 </div>
