73737774077456614dae41adddb2e8d83a6d2efa
amin havasi <pau.ahq@gmail.com>
2020-05-29 14:58:26 +0430

    not found page

--- src/components/notFound.js
+++ src/components/notFound.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
-    return <div>NotFound Page</div>;
+    return (
+        <div
+            style={{ minHeight: '100vh' }}
+            className="bg-dark d-flex justify-content-center align-items-center text-white"
+        >
+            <div className="text-center">
+                <p className="display-4">Opps!!</p>
+                <p>page not found!</p>
+                <Link className="btn btn-primary btn-sm" to="/">
+                    Go Back to HomePage
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default NotFound;
