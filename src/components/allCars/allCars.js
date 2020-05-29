810434979480447420f9db4ad3cee9088fbb8bfb
amin havasi <pau.ahq@gmail.com>
2020-05-29 17:09:12 +0430

    main cars

--- src/components/allCars/allCars.js
+++ src/components/allCars/allCars.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import NavBar from './navBar';
+import Footer from '../common/footer';
 
 const AllCars = () => {
     return (
         <React.Fragment>
             <NavBar />
+            <main style={{ minHeight: '100vh', backgroundColor: 'white' }}>
+                no post for see
+            </main>
+            <Footer />
         </React.Fragment>
     );
 };
