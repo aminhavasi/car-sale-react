e65f29dd96fab5d1256824b09641b5f45e1752e4
amin havsi <pau.ahq@gmail.com>
2020-06-07 09:13:23 -0700

    'df'

--- src/components/notFound.js
+++ src/components/notFound.js
@@ -2,20 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFound = () => {
-    return (
-        <div
-            style={{ minHeight: '100vh' }}
-            className="bg-dark d-flex justify-content-center align-items-center text-white"
-        >
-            <div className="text-center">
-                <p className="display-4">Opps!!</p>
-                <p>page not found!</p>
-                <Link className="btn btn-primary btn-sm" to="/">
-                    Go Back to HomePage!
-                </Link>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      style={{ minHeight: '100vh' }}
+      className="bg-dark d-flex justify-content-center align-items-center text-white"
+    >
+      <div className="text-center">
+        <p className="display-4">Opps!!</p>
+        <p>page not found!</p>
+        <Link className="btn btn-primary btn-sm" to="/">
+          Go Back to HomePage
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
